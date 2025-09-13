c57260eb3bb471f6f0eb6f213bebdfb1f43fab41
abdulaziz-go <abdulaziomonov4@gmail.com>
2025-09-13 16:08:49 +0500

    decoder and encoder done

--- storage/file.go
+++ storage/file.go
@@ -1,1 +1,21 @@
 package storage
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type LogFile struct {
+	file   *os.File
+	path   string
+	offset int64
+}
+
+func NewLogFile(dirPath string, fileID int) (*LogFile, error) {
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		return nil, fmt.Errorf("failed to create directory %v", err)
+	}
+	filepath.Join(dirPath, fmt.Sprintf("bitcask_%d.log", fileID))
+	return nil, nil
+}
--- storage/record.go
+++ storage/record.go
@@ -1,1 +1,102 @@
 package storage
+
+import "errors"
+
+type Record struct {
+	Timestamp int64 // 8 byte
+	Key       string
+	Value     []byte
+	Size      int64
+}
+
+func (r *Record) Encode() []byte {
+	keyBytes := []byte(r.Key)
+	keySize := len(keyBytes)
+	valueSize := len(r.Value)
+
+	totalSize := 8 + 4 + 4 + keySize + valueSize
+	buf := make([]byte, totalSize)
+
+	pos := 0
+
+	u64 := uint64(r.Timestamp)
+	buf[pos] = byte(u64 >> 56)
+	buf[pos+1] = byte(u64 >> 48)
+	buf[pos+2] = byte(u64 >> 40)
+	buf[pos+3] = byte(u64 >> 32)
+	buf[pos+4] = byte(u64 >> 24)
+	buf[pos+5] = byte(u64 >> 16)
+	buf[pos+6] = byte(u64 >> 8)
+	buf[pos+7] = byte(u64)
+	pos += 8
+
+	u32 := uint32(keySize)
+	buf[pos] = byte(u32 >> 24)
+	buf[pos+1] = byte(u32 >> 16)
+	buf[pos+2] = byte(u32 >> 8)
+	buf[pos+3] = byte(u32)
+	pos += 4
+
+	u32 = uint32(valueSize)
+	buf[pos] = byte(u32 >> 24)
+	buf[pos+1] = byte(u32 >> 16)
+	buf[pos+2] = byte(u32 >> 8)
+	buf[pos+3] = byte(u32)
+	pos += 4
+
+	// Key copy
+	copy(buf[pos:pos+keySize], keyBytes)
+	pos += keySize
+
+	// Value copy
+	copy(buf[pos:pos+valueSize], r.Value)
+
+	r.Size = int64(totalSize)
+	return buf
+}
+
+func (r *Record) Decoder(data []byte) (*Record, error) {
+	if len(data) > 16 {
+		return nil, errors.New("timestamp , keysize and value size mismatch")
+	}
+
+	pos := 0
+
+	timestamp := int64(uint64(data[pos])<<56 |
+		uint64(data[pos+1])<<48 |
+		uint64(data[pos+2])<<40 |
+		uint64(data[pos+3])<<32 |
+		uint64(data[pos+4])<<24 |
+		uint64(data[pos+5])<<16 |
+		uint64(data[pos+6])<<8 |
+		uint64(data[pos+7]))
+	pos += 8
+	keySize := int(uint32(data[pos])<<24 |
+		uint32(data[pos+1])<<16 |
+		uint32(data[pos+2])<<8 |
+		uint32(data[pos+3]))
+	pos += 4
+	valueSize := int(uint32(data[pos])<<24 |
+		uint32(data[pos+1])<<16 |
+		uint32(data[pos+2])<<8 |
+		uint32(data[pos+3]))
+	pos += 4
+
+	if len(data) < pos+keySize+valueSize {
+		return nil, errors.New("data byte mismatch with pos , key and valuesize")
+	}
+
+	key := string(data[pos : pos+keySize])
+	pos += keySize
+
+	value := make([]byte, valueSize)
+	copy(value, data[pos:pos+valueSize])
+
+	record := &Record{
+		Timestamp: timestamp,
+		Key:       key,
+		Value:     value,
+		Size:      int64(16 + keySize + valueSize),
+	}
+	return record, nil
+}
